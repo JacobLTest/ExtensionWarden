6c787c93a183569df1d63b1227dee6ead47e13be
JacobLTest <jacobklenes@gmail.com>
2025-05-19 12:18:20 -0400

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type {Metadata} from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import './globals.css';
+import { Toaster } from "@/components/ui/toaster";
+
+export const metadata: Metadata = {
+  title: 'Extension Warden',
+  description: 'Manage and discover IDE extensions.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`dark ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body className="font-mono antialiased">
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
